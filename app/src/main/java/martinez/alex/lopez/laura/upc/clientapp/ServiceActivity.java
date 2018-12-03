1aa97b4fcefecddddafdca73427757b7d6f14189
alex.martinez.martinez <alex.martinez.upc@gmail.com>
2018-12-03 11:25:11 +0100

    V0.1.0: Disseny del layout de l’activitat ServiceActivity.

--- app/src/main/java/martinez/alex/lopez/laura/upc/clientapp/ServiceActivity.java
+++ app/src/main/java/martinez/alex/lopez/laura/upc/clientapp/ServiceActivity.java
@@ -2,6 +2,7 @@ package martinez.alex.lopez.laura.upc.clientapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ServiceActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class ServiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+    }
+
+    public void onClickChooseMaterial(View view) {
+    }
+
+    public void onClickChooseTime(View view) {
+    }
+
+    public void onClickChooseDate(View view) {
+    }
+
+    public void onClickChooseHour(View view) {
     }
 }
