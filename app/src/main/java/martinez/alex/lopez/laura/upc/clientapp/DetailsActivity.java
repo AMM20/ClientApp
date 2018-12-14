13584b95d010e17a28b511a8e1803998ddc54303
alex.martinez.martinez <alex.martinez.upc@gmail.com>
2018-12-14 13:39:29 +0100

    V0.3.1  Permetre la introducció de les dades del client i guardar-les a la reserva.

--- app/src/main/java/martinez/alex/lopez/laura/upc/clientapp/DetailsActivity.java
+++ app/src/main/java/martinez/alex/lopez/laura/upc/clientapp/DetailsActivity.java
@@ -10,12 +10,76 @@ import android.widget.EditText;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private Reserva reserva;
+    private Client client;
+
+    private EditText EditName;
+    private EditText EditLastName;
+    private EditText EditEmail;
+    private EditText EditPhone;
+    private EditText EditNotes;
+
+    private boolean fieldsChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        Intent intent = getIntent();
+        reserva = (Reserva) intent.getSerializableExtra("reserva");
+
+        client = new Client();
+
+        EditName = findViewById(R.id.EditName);
+        EditLastName = findViewById(R.id.EditLastName);
+        EditEmail = findViewById(R.id.EditEmail);
+        EditPhone = findViewById(R.id.EditPhone);
+        EditNotes = findViewById(R.id.EditNotes);
+
     }
 
+    public void onClickConfirm(View view) {
+
+        OmpleClient();
+
+        if (fieldsChecked) {
+            reserva.setClient(client);
+            //Intent intent = new Intent(this,SummaryActivity.class);
+            //intent.putExtra("reserva", reserva);
+            //startActivityForResult(intent,0);
+        }else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.missing_statements_message);
+
+            builder.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    return;
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+    private void OmpleClient() {
+
+        fieldsChecked = true;
+
+        client.setName(String.valueOf(EditName.getText()));
+        if (client.getName().equals("")) fieldsChecked = false;
+
+        client.setLastName(String.valueOf(EditLastName.getText()));
+        if (client.getLastName().equals("")) fieldsChecked = false;
+
+        client.setEmail(String.valueOf(EditEmail.getText()));
+        if (client.getEmail().equals("")) fieldsChecked = false;
+
+        if (String.valueOf(EditPhone.getText()).equals("")) fieldsChecked = false;
+        else client.setPhone(Integer.parseInt(String.valueOf(EditPhone.getText())));
+
+        client.setNotes(String.valueOf(EditNotes.getText()));
+
+    }
 }
